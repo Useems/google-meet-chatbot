3fd9198c7e0ac19b0dd6dcbcfd6e84920fff55be
Marcos André <34917473+Useems@users.noreply.github.com>
2020-06-15 12:49:48 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+// Sample Code
+
+function onMessage(data) {
+	let prefix = data.message.slice(0, 1);
+	let splitted = data.message.slice(1).toLowerCase().split(' ');
+	
+	if (prefix == '.') {
+		switch (splitted[0]) {
+			case 'ping':
+				sendMessage('Pong!');
+				break;
+			case 'calc':
+				let expression = splitted.slice(1).join('').replace(/[^-()\d/*+.]/g, '');
+				let value = eval(expression); 
+				
+				if (value)
+					sendMessage('Expression:  ', expression, '\nResult: ', value);
+				break;
+			case 'hi':
+				sendMessage('Hello, ', data.username, '!');
+				break;
+			case 'invite':
+				sendMessage('Invite URL: ', window.location.href.split('?')[0]);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+// Framework Code
+
+(() => {
+	let message = document.querySelector('div > div > div:nth-child(4) > div > div > div > div > div > div > span:nth-child(2) > div > div > div > div > div > textarea');
+	let button = document.querySelector('div > div > div:nth-child(4) > div > div > div > div > div > div > span:nth-child(2) > div > div:nth-child(3) > div:nth-child(2)');
+
+	let messagesLength = -1;
+	let lastMessageLength = -1;
+
+	window.sendMessage = function(...args) {		
+		message.value = args.join('');
+		button.ariaDisabled = null
+		button.click();
+	}
+
+	if (window.messagesInterval)
+		try {
+			clearInterval(messagesInterval)
+		} catch (err) {}
+
+	window.messagesInterval = setInterval(() => {
+		var messages = document.querySelector('div > div > div:nth-child(4) > div > div > div > div > div > div > span:nth-child(2) > div > div').childNodes;
+		
+		if (messages.length > 0)
+			var message = messages[messages.length - 1];
+
+		if (messagesLength === -1) {
+			messagesLength = messages.length;
+			lastMessageLength = message ? message.childNodes[1].childNodes.length : 0;
+		} else if (messagesLength != messages.length || (message && lastMessageLength != message.childNodes[1].childNodes.length)) {
+			let messageText = message.childNodes[1].childNodes[message.childNodes[1].childNodes.length - 1].dataset.messageText;
+
+			messagesLength = messages.length;
+			lastMessageLength = message.childNodes[1].childNodes.length;
+
+			if (onMessage && typeof(onMessage) == 'function') {
+				onMessage({
+					username: message.dataset.senderName,
+					message: messageText,
+					date: parseInt(message.dataset.timestamp)
+				});
+			}
+		}
+	}, 100);
+})();
